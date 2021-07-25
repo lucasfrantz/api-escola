ec29f8c7f235d8eaae4759a31474c517176a57dd
Lucas Frantz <maxkronus@gmail.com>
2021-07-25 09:40:39 -0300

    rotas de niveis,turmas e matriculas

--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -8,6 +8,10 @@ router.get('/pessoas/:id', PessoaController.pegaUmaPessoa)
 router.post('/pessoas', PessoaController.criarPessoa)
 router.put('/pessoas/:id', PessoaController.atualizaPessoa)
 router.delete('/pessoas/:id', PessoaController.apagaPessoa)
+router.get('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.pegaUmaMatricula)
+router.post('/pessoas/:estudanteId/matricula', PessoaController.criarMatricula)
+router.put('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.atualizaMatricula)
+router.delete('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.apagaMatricula)
 
 
 module.exports = router
