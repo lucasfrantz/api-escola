c38c95526443dc255c622df4398aca73ed95286e
Lucas Frantz <maxkronus@gmail.com>
2021-07-25 20:35:30 -0300

    services

---
+++ api/services/MatriculasServices.js
@@ -0,0 +1,11 @@
+const Services = require('./Services')
+
+class MatriculasServices extends Services {
+    constructor() {
+        super('Matriculas')
+    }
+    //METODOS ESPECIFICOS
+
+}
+
+module.exports = MatriculasServices
---
+++ api/services/TurmasServices.js
@@ -0,0 +1,14 @@
+const Services = require('./Services')
+const Sequelize = require('sequelize')
+const Op = Sequelize.Op
+
+class TurmasServices extends Services {
+    constructor() {
+        super('Turmas')
+    }
+    //METODOS ESPECIFICOS
+
+
+}
+
+module.exports = TurmasServices
