dde576941f4a85e7e2d70e7f66f424b7f8b7b820
Lucas Frantz <maxkronus@gmail.com>
2021-07-25 17:42:09 -0300

    agregadores

--- api/models/pessoas.js
+++ api/models/pessoas.js
@@ -11,18 +11,49 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       Pessoas.hasMany(models.Turmas, { foreignKey: 'docente_id' })
-      Pessoas.hasMany(models.Matriculas, { foreignKey: 'estudante_id' })
+      Pessoas.hasMany(models.Matriculas, {
+        foreignKey: 'estudante_id',
+        scope: {
+          status: 'confirmado'
+        },
+        as: 'aulasMatriculadas'
+      })
     }
   };
   Pessoas.init({
-    nome: DataTypes.STRING,
+    nome: {
+      type: DataTypes.STRING,
+      validate: {
+        funcaoValidadora: function (dado) {
+          if (dado.length <= 3) throw new Error('O campo nome deve ter mais de 3 caracteres')
+        }
+      }
+    },
     ativo: DataTypes.BOOLEAN,
-    email: DataTypes.STRING,
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        isEmail: {
+          args: true,
+          msg: 'dados do tipo email invalidos'
+        }
+      }
+    },
     role: DataTypes.STRING
   }, {
     sequelize,
     modelName: 'Pessoas',
     paranoid: true,
+    defaultScope: {
+      where: {
+        ativo: true
+      }
+    },
+    scopes: {
+      todos: {
+        where: {}
+      }
+    }
   });
   return Pessoas;
 };
--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -4,16 +4,20 @@ const PessoaController = require('../controllers/PessoaController')
 const router = Router()
 
 router
-    .get('/pessoas', PessoaController.pegaTodasAsPessoas)
+    .get('/pessoas', PessoaController.pegaPessoasAtivas)
+    .get('/pessoas/todos', PessoaController.pegaTodasAsPessoas)
     .get('/pessoas/:id', PessoaController.pegaUmaPessoa)
     .post('/pessoas', PessoaController.criarPessoa)
     .put('/pessoas/:id', PessoaController.atualizaPessoa)
     .delete('/pessoas/:id', PessoaController.apagaPessoa)
     .post('/pessoas/:id/restaura', PessoaController.restauraPessoa)
     .get('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.pegaUmaMatricula)
+    .get('/pessoas/matricula/lotada', PessoaController.pegaTurmasLotadas)
     .post('/pessoas/:estudanteId/matricula', PessoaController.criarMatricula)
+    .get('/pessoas/:estudanteId/matricula', PessoaController.pegaMatriculas)
     .put('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.atualizaMatricula)
     .delete('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.apagaMatricula)
     .post('/pessoas/:estudanteId/matricula/:matriculaId/restaura', PessoaController.restauraMatricula)
+    .get('/pessoas/matricula/:turmaId/confirmadas', PessoaController.pegaMatriculasPorTurma)
 
 module.exports = router
