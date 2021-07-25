28f161b8043246538f6f338a5b8e16ec3f1f5912
Lucas Frantz <maxkronus@gmail.com>
2021-07-25 11:58:03 -0300

    soft delete

--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -3,15 +3,17 @@ const PessoaController = require('../controllers/PessoaController')
 
 const router = Router()
 
-router.get('/pessoas', PessoaController.pegaTodasAsPessoas)
-router.get('/pessoas/:id', PessoaController.pegaUmaPessoa)
-router.post('/pessoas', PessoaController.criarPessoa)
-router.put('/pessoas/:id', PessoaController.atualizaPessoa)
-router.delete('/pessoas/:id', PessoaController.apagaPessoa)
-router.get('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.pegaUmaMatricula)
-router.post('/pessoas/:estudanteId/matricula', PessoaController.criarMatricula)
-router.put('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.atualizaMatricula)
-router.delete('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.apagaMatricula)
-
+router
+    .get('/pessoas', PessoaController.pegaTodasAsPessoas)
+    .get('/pessoas/:id', PessoaController.pegaUmaPessoa)
+    .post('/pessoas', PessoaController.criarPessoa)
+    .put('/pessoas/:id', PessoaController.atualizaPessoa)
+    .delete('/pessoas/:id', PessoaController.apagaPessoa)
+    .post('/pessoas/:id/restaura', PessoaController.restauraPessoa)
+    .get('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.pegaUmaMatricula)
+    .post('/pessoas/:estudanteId/matricula', PessoaController.criarMatricula)
+    .put('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.atualizaMatricula)
+    .delete('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.apagaMatricula)
+    .post('/pessoas/:estudanteId/matricula/:matriculaId/restaura', PessoaController.restauraMatricula)
 
 module.exports = router
